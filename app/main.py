c8ac29b354cf694f19e7ae587c2ffd3da5f57f0a
JohnLool <pinaev-misha-pinaev@yandex.ru>
2025-04-01 19:42:28 +0500

    docker-compose config updated

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 # from starlette_exporter import PrometheusMiddleware, handle_metrics
 
@@ -26,18 +25,6 @@ async def unique_constraint_exception_handler(request: Request, exc: UniqueConst
         content={"detail": exc.message}
     )
 
-origins = [
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # app.add_middleware(PrometheusMiddleware)
 # app.add_route("/metrics", handle_metrics)
